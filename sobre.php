e228747b7dcabb1a6b38e7c1fb203b598f7b3f6f
Onnie <brasil_connectint@hotmail.com>
2019-08-15 17:17:10 -0300

    Alterei sobre nós mais irei reformular tudo de novo

--- sobre.php
+++ sobre.php
@@ -9,91 +9,98 @@
 </head>
 <body>
 
-	<nav class="navbar navbar-expand-lg navbar-dark text-white m-0 py-4" style="background-color: rgba(15, 116, 163, 0.6);">
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-dark text-white m-0 py-4">
 		<div class="container-fluid">
+			<p class="font-weight-light h4">ComfyCare</p>
 			<div class="container">
 				<div class="row">
-					<div class="col-4 text-left">
-						<p class="font-weight-light h4">CUIDADOS</p>
-					</div>
-
-					<div class="col-8 text-right">
+					<div class="col-9 offset-md-3 text-right">
 						<ul class="navbar-nav float-left pt-1">
-							<li class="nav-item mr-4"><a href="index.php" class="nav-link p-0">Home</a></li>
-							<li class="nav-item active mr-4"><a href="sobre.php" class="nav-link p-0">Sobre nós</a></li>
-							<li class="nav-item mr-4"><a href="blog.php" class="nav-link p-0">Blog</a></li>
-							<li class="nav-item mr-4"><a href="contato.php" class="nav-link p-0">Contate-nos</a></li>
+							<li class="nav-item mr-5"><a href="sobre.php" class="nav-link p-0">Quem somos</a></li>
+							<li class="nav-item mr-5"><a href="blog.php" class="nav-link p-0">Serviços</a></li>
+							<li class="nav-item mr-5"><a href="contato.php" class="nav-link p-0">Como funciona</a></li>
+							<li class="nav-item mr-5"><a href="contato.php" class="nav-link p-0">Dúvidas</a></li>
+							<li class="nav-item mr-5"><a href="contato.php" class="nav-link p-0">Blog</a></li>
 							<li class="nav-item mr-4"><a href="cadastro_cuidador.php" class="nav-link p-0">Trabalhe conosco</a></li>
 						</ul>
-						<div class="float-right">
-							<a href="login.php" class="btn btn-outline-light btn-sm mr-3">Login</a>
-							<a href="cadastro.php" class="btn btn-outline-light btn-sm">Cadastre-se</a>
-						</div>
 					</div>
 				</div>
 			</div>
+			<a href="login.php" class="btn btn-outline-light btn-sm mr-3">Login</a>
+			<a href="cadastro.php" class="btn btn-outline-light btn-sm">Cadastro</a>
 		</div>
 	</nav>
 
 	<div class="container-fluid">
 		<div class="container">
 			<div class="row">
-				<div class="col-12">
-					<div class=" my-5">
-						<!-- <div class="card-body">
- -->							<p class="h1 font-weight-normal mb-4">
-								Sobre nós
-								<span></span>
-							</p>
-							<div class="row no-gutters h-50">
-								<div class="col-6 col-md-4 pl-0">
-									<img src="Penguins.jpg" height="274" width="311">
-								</div>
-								<div class="col-12 col-sm-6 col-md-7 pl-0 text-justify my-auto">
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-									proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-								</div>
-							</div>
-							<div class="row no-gutters h-50">
-								<div class="col-12 col-sm-6 col-md-7 pt-5 pl-0 text-justify my-auto">
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-									proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-								</div>
-								<div class="col-6 col-md-3 pl-5 pt-5">
-									<img src="Penguins.jpg" height="274" width="311">
-								</div>
-							</div>
+				<div class="col-12">							
+					<p class="h1 font-weight-normal mb-4">
+						Sobre nós
+						<span></span>
+					</p>
+					<div class="row no-gutters">
+						<div class="col-4">
+							<img src="img/blablabla.png" height="274" width="351">
+						</div>
+						<div class="col-8 text-justify my-auto" style="font-size: 3;">
+							<p class="h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						</div>
+					</div>
+					<div class="row no-gutters">
+						<div class="col-8 text-justify my-auto">
+							<p class="h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						</div>
+						<div class="col-4">
+							<img src="img/boxhome2.jpg" height="274" width="351" style="float: right;">
+						</div>
+					</div>
+					<div class="row no-gutters">
+						<div class="col-4 mb-4">
+							<img src="img/file.jpg" height="274" width="351">
+						</div>
+						<div class="col-8 mb-4 text-justify my-auto">
+							<p class="h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 						</div>
 					</div>
 				</div>
-				<div class="col-6">
-					
+			</div>
+			<div class="col-6">
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<footer class="text-center text-white py-5" style="background-color: rgba(15, 116, 163, 0.6);">
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<p class="m-0">&copy; Direitos reservados</p>
 				</div>
 			</div>
 		</div>
 	</div>
+</footer>
 
-	<footer class="text-center text-white py-5" style="background-color: rgba(15, 116, 163, 0.6);">
-		<div class="container-fluid">
-			<div class="container">
-				<div class="row">
-					<div class="col-12">
-						<p class="m-0">&copy; Direitos reservados</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
-
-	<!-- JS | Bootstrap -->
-	<script src="bootstrap/bootstrap.min.js"></script>
+<!-- JS | Bootstrap -->
+<script src="bootstrap/bootstrap.min.js"></script>
 </body>
 </html>
