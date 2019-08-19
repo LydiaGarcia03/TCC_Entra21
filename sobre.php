020cb5dbf939382e06689ef6bcdb362064e89159
Lydia Garcia <lydiagarcia.chan@gmail.com>
2019-08-19 15:19:07 -0300

    Adição do sobre atualizado

--- sobre.php
+++ sobre.php
@@ -2,12 +2,25 @@
 <html lang="pt-br">
 <head>
 	<meta charset="utf-8">
-	<title>ComfyCare | Sobre</title>
+	<title>ComfyCare | Sobre nós</title>
 	<!-- CSS | Bootstrap -->
 	<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="style/style.css">
+
+	<style type="text/css">
+	#imagem {
+		width: 1262px;
+		height: 500px;
+	}
+	#texto {
+		position: absolute;
+		margin-top: -497px;
+		color: white;
+		font-family: arial;
+	}
+</style>
 </head>
-<body>
+<body style="background-color: rgba(15, 116, 163, 0.1)">
 
 	<!-- Navbar -->
 	<nav class="navbar navbar-expand-lg navbar-dark text-white m-0 py-4">
@@ -15,12 +28,12 @@
 			<a href="index.php" style="text-decoration: none;"><p class="font-weight-light text-white h4">ComfyCare</p></a>
 			<div class="container">
 				<div class="row">
-					<div class="col-10 col-sm-12 offset-md-3 text-right">
+					<div class="col-10 offset-md-3 text-right">
 						<ul class="navbar-nav float-left pt-1">
 							<li class="nav-item active mr-5"><a href="sobre.php" class="nav-link p-0">Quem somos</a></li>
 							<li class="nav-item mr-5"><a href="blog.php" class="nav-link p-0">Serviços</a></li>
-							<li class="nav-item mr-5"><a href="como_funciona.php" class="nav-link p-0">Como funciona</a></li>
-							<li class="nav-item mr-5"><a href="FAQ.php" class="nav-link p-0">Dúvidas</a></li>
+							<li class="nav-item mr-5"><a href="contato.php" class="nav-link p-0">Como funciona</a></li>
+							<li class="nav-item mr-5"><a href="contato.php" class="nav-link p-0">Dúvidas</a></li>
 							<li class="nav-item mr-5"><a href="contato.php" class="nav-link p-0">Blog</a></li>
 							<li class="nav-item mr-5"><a href="cadastro_cuidador.php" class="nav-link p-0">Trabalhe conosco</a></li>
 						</ul>
@@ -31,76 +44,83 @@
 			<a href="cadastro.php" class="btn btn-outline-light btn-sm">Cadastro</a>
 		</div>
 	</nav>
-
+	<img id="imagem" src="Penguins.jpg" width="1263" height="500">
 	<div class="container-fluid">
 		<div class="container">
 			<div class="row">
-				<div class="col-12">							
-					<p class="h1 font-weight-normal mb-4">
-						Sobre nós
-						<span></span>
-					</p>
-					<div class="row no-gutters">
-						<div class="col-4">
-							<img src="img/blablabla.png" height="274" width="351">
-						</div>
-						<div class="col-8 text-justify my-auto" style="font-size: 3;">
-							<p class="h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						</div>
-					</div>
-					<div class="row no-gutters">
-						<div class="col-8 text-justify my-auto">
-							<p class="h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						</div>
-						<div class="col-4">
-							<img src="img/boxhome2.jpg" height="274" width="351" style="float: right;">
-						</div>
-					</div>
-					<div class="row no-gutters">
-						<div class="col-4 mb-4">
-							<img src="img/file.jpg" height="274" width="351">
-						</div>
-						<div class="col-8 mb-4 text-justify my-auto">
-							<p class="h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-6">
-
-			</div>
-		</div>
-	</div>
-</div>
-
-<footer class="text-center text-white py-5" style="background-color: rgba(15, 116, 163, 0.6);">
-	<div class="container-fluid">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<p class="m-0">&copy; Direitos reservados</p>
+				<div class="col-12 mt-3 mb-3">
+					<h1 id="texto">Sobre nós</h1>
 				</div>
 			</div>
 		</div>
 	</div>
-</footer>
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">	
+					<div class="card mb-3">
+						<div class="row no-gutters">
+							<div class="col-md-4">
+								<img src="img/blablabla.png" class="card-img" alt="...">
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card mb-3">
+						<div class="row no-gutters">
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<img src="img/blablabla.png" class="card-img" alt="...">
+							</div>
+						</div>
+					</div>
+					<div class="card mb-3">
+						<div class="row no-gutters">
+							<div class="col-md-4">
+								<img src="img/blablabla.png" class="card-img" alt="...">
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-6">
 
-<!-- JS | Bootstrap -->
-<script src="bootstrap/bootstrap.min.js"></script>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer class="text-center text-white py-5" style="background-color: rgba(15, 116, 163, 0.6);">
+		<div class="container-fluid">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<p class="m-0">&copy; Direitos reservados</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<!-- JS | Bootstrap -->
+	<script src="bootstrap/bootstrap.min.js"></script>
 </body>
 </html>
