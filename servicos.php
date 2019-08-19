b2398543b797338937ead0682e540c236b6abe04
Lydia Garcia <lydiagarcia.chan@gmail.com>
2019-08-19 16:49:26 -0300

    Alteração das navbars(leve atualização)

--- servicos.php
+++ servicos.php
@@ -15,10 +15,10 @@
 			<a href="index.php" style="text-decoration: none;"><p class="font-weight-light text-white h4">ComfyCare</p></a>
 			<div class="container">
 				<div class="row">
-					<div class="col-10 col-sm-12 offset-md-3 text-right">
+					<div class="col-10 col-sm-9 offset-md-3 text-right">
 						<ul class="navbar-nav float-left pt-1">
 							<li class="nav-item mr-5 mr-sm-4"><a href="sobre.php" class="nav-link p-0">Quem somos</a></li>
-							<li class="nav-item active mr-5 mr-sm-4"><a href="servicos.php" class="nav-link p-0">Serviços</a></li>
+							<li class="nav-item active mr-5 mr-sm-4"><a href="#servicos.php" class="nav-link p-0">Serviços</a></li>
 							<li class="nav-item mr-5 mr-sm-4"><a href="como_funciona.php" class="nav-link p-0">Como funciona</a></li>
 							<li class="nav-item mr-5 mr-sm-4"><a href="FAQ.php" class="nav-link p-0">Dúvidas</a></li>
 							<li class="nav-item mr-5 mr-sm-4"><a href="blog.php" class="nav-link p-0">Blog</a></li>
