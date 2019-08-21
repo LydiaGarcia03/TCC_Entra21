fce7bb2ff1dd1bb4ca335dfe4489573c2b18d835
João Victor <brasil_connectint@hotmail.com>
2019-08-21 17:13:08 -0300

    Atualizando o cadastro do Cuidador By Onnie, ps: ainda não ta pronto :p

--- cadastro_cuidador.php
+++ cadastro_cuidador.php
@@ -14,26 +14,24 @@
 			<div class="card-header">Crie sua conta grátis</div>
 			<div class="card-body">
 				<form>
-					<div class="form-group">
+					<form id="formExemplo" data-toggle="validator" role="form">
+					<div class="form-group ">
 						<div class="form-row">
-							<div class="col-md-6">
+							<div class="col-md-12">
 								<div class="form-label-group">
-									<input type="text" id="primeiroNome" class="form-control" 
-									placeholder="Primeiro nome" required="required" autofocus="autofocus">    
-								</div>
-							</div>
-							<div class="col-md-6">
-								<div class="form-label-group">
-									<input type="text" id="ultimoNome" class="form-control" 
-									placeholder="Último nome" required="required">    
+									<input type="text" id="NomeComp" class="form-control" 
+									placeholder="Nome completo" required="required" autofocus="autofocus">
+									<div class="help-block with-errors"></div>    
 								</div>
 							</div>
 						</div>
 					</div>
+
 					<div class="form-group">
 						<div class="form-label-group">
-							<input type="email" id="email" class="form-control" 
-							placeholder="Endereço de e-mail" required="required">
+							<input type="inputEmail" id="email" class="form-control" 
+							placeholder="Endereço de e-mail" required="required"  data-error="Por favor, informe um e-mail correto." >
+							<div class="help-block with-errors"></div>
 						</div>
 					</div>
 					<div class="form-group">
@@ -41,31 +39,60 @@
 							<div class="col-md-6">
 								<div class="form-label-group">
 									<input type="password" id="Senha" class="form-control" 
-									placeholder="Senha" required="required">    
+									placeholder="Digite a senha minimo 8 digitos" required="required"data-minlength="8" required>
+									<div class="help-block with-errors"></div>    
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-label-group">
 									<input type="password" id="ConfirmaSenha" class="form-control" 
-									placeholder="Confirma Senha" required="required">    
+									placeholder="Confirma Senha" required="required">    <div class="help-block with-errors"></div>
+								</div>
+							</div>
+							<div class="col-md-6 mt-3">
+								<div class="form-label-group">
+									<input type="" id="cell" class="form-control" 
+									placeholder="Numero de Telefone" required="required" pattern="[0-9]+$" > 
+									<div class="help-block with-errors"></div>   
+								</div>
+							</div>
+							<div class="col-md-6 mt-3">
+								<div class="form-label-group">
+									<input type="" id="Core" class="form-control" 
+									placeholder="Digite seu numero de Coren" required="required" pattern="[0-9]+$" >  
+									<div class="help-block with-errors"></div>  
+								</div>
+							</div>
+							<div class="col-md-6 mt-3">
+								<div class="form-label-group">
+									<input type="" id="Cep" class="form-control" 
+									placeholder="Digite seu Cep" required="required" pattern="[0-9]+$" >  
+									<div class="help-block with-errors"></div>  
 								</div>
 							</div>
 						</div>
 					</div>
-					<a class="btn btn-primary btn-block" href="">Criar Conta</a>
-				</form>
-				<div class="text-center">
-					<a class="d-block small mt-3" href="">Página de login</a>
-					<a class="d-block small" href="">Esqueceu sua senha?</a>
+
+					<div class="custom-control custom-checkbox
+					mb-3">
+					<input type="checkbox" class="custom-control-input" id="customCheck1" required="required">
+					<label class="custom-control-label" for="customCheck1">De acordo com os dados coletados?</label>
 				</div>
+				<a class="btn btn-primary btn-block" href="">Criar Conta</a>
+			</form>
+			<div class="text-center">
+				<a class="d-block small mt-3" href="">Página de login</a>
+				<a class="d-block small" href="">Esqueceu sua senha?</a>
 			</div>
 		</div>
-	</div>  
+	</div>
+</div>  
+</form>
 
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
