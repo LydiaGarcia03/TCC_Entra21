67a7704cadb3394a96096fc80a28443d03bf195b
João Victor <brasil_connectint@hotmail.com>
2019-09-16 17:16:46 -0300

    Terminando o grafico

--- dashboard_cuidador.php
+++ dashboard_cuidador.php
@@ -239,12 +239,12 @@
 							</div>
 
 							<!-- Area Chart -->
-							<div class="card shadow mb-4">
+							<div class="card shadow mb-4 mt-3">
 								<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between" >
 									<h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
 								</div>
 
-								<div class="card-body" style="height: 320px; width: 100%;">
+								<div class="card-body" style="height: 360px; width: 100%;">
 									<div class="chart-area">
 										<canvas id="myAreaChart" ></canvas>
 									</div>
