96b36298899ca6bc3a22753ccb35a1fef7229653
Marcelo Sales Gonzaga <sallesgonzaga@outlook.com>
2019-09-17 16:50:47 -0300

    Revert "Quase finalizado blog e FAQ"
    This reverts commit 944baae53ea61f061a5c6f2e4a1177f1029909bd.

--- blog.php
+++ blog.php
@@ -1,192 +1,90 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="pt-br">
 
 <head>
-    <title> </title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Blog</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- CSS -->
+	<link href="style/home.css" rel="stylesheet" type="text/css">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-
-    <!-- css -->
-    <link rel="stylesheet" href="style/stylo_blog.css">
+	<link rel="stylesheet" type="text/css" href="style/blog.css">
 </head>
 
-<body>
-    <!-- Header -->
-    <header>
-        <div class="container-fluid p-0">
-            <nav class="navbar navbar-expand-lg">
-                <a class="navbar-brand logo" href="index.php">
-                    <i class="fas fa-laptop-medical fa-2x mx-3"></i>HCDigital</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-align-right text-light"></i>
-                </button>                
-            </nav>
-        </div>
-    </header>
+<body class="bg-light">
 
-    <section class="section-2">
-        <div class="container">
-            <div class="row ">
-                <div class="card mb-3" style=";">
-                  <div class="row no-gutters">
-                    <div class="col-md-5">
-                      <img src="img/blog-5.jpg" class="card-img" alt="...">
-                  </div>
-                  <div class="col-md-7">
-                      <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
-                        <p class="card-text texto-pe"><small class="text-muted ">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</section>
+	<!-- NAVBAR -->
+	<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+		<div class="container">
 
+			<a href="index.php" class="navbar-brand h1 mb-0"><i class="fas fa-heartbeat pr-2"></i>HCDigital</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
-<section>
-    <div class="container">
-        <div class="row ">
-            <div class="card-deck">
-              <div class="card"> 
-                <a href="">
-                    <img src="img/blog-1.jpg" class="card-img-top" alt="...">
-                </a>
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              </div>
-              <div class="card-footer">
-                  <small class="text-muted tema">Saúde</small>
-                  <small class="text-muted data ">21/09/2019</small>
-              </div>
-          </div>
-          <div class="card">
-            <a href="">
-                <img src="img/blog-2.jpg" class="card-img-top" alt="...">
-            </a>
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          </div>
-          <div class="card-footer">
-              <small class="text-muted tema">Saúde</small>
-              <small class="text-muted data ">21/09/2019</small>
-          </div>
-      </div>
-      <div class="card">
-        <a href="">
-            <img src="img/blog-3.jpg" class="card-img-top" alt="...">
-        </a>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted tema">Saúde</small>
-        <small class="text-muted data ">21/09/2019</small>              
-    </div>
-</div>
-</div>
-</div>
+			<div class="collapse navbar-collapse ml-5" id="navbarSite">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a href="quem_somos.php" class="nav-link">QUEM SOMOS</a>
+					</li>
+					<li class="nav-item">
+						<a href="duvidas.php" class="nav-link">DÚVIDAS</a>
+					</li>
+					<li class="nav-item active">
+						<a href="blog.php" class="nav-link">BLOG</a>
+					</li>
+					<li class="nav-item">
+						<a href="index.php#tbConosco" class="nav-link">TRABALHE CONOSCO</a>
+					</li>
+				</ul>
+			</div>
+			
+			<a href="login.php" class="btn btn-outline-light btn-sm mr-3">Login</a>
+			<a href="cadastro_usuario.php" class="btn btn-outline-light btn-sm">Cadastro</a>
 
-<div class="row ">
-    <div class="card-deck">
-      <div class="card"> 
-        <a href="">
-            <img src="img/blog-1.jpg" class="card-img-top" alt="...">
-        </a>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div class="card-footer">
-          <small class="text-muted tema">Saúde</small>
-          <small class="text-muted data ">21/09/2019</small>
-      </div>
-  </div>
-  <div class="card">
-    <a href="">
-        <img src="img/blog-2.jpg" class="card-img-top" alt="...">
-    </a>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-  </div>
-  <div class="card-footer">
-      <small class="text-muted tema">Saúde</small>
-      <small class="text-muted data ">21/09/2019</small>
-  </div>
-</div>
-<div class="card">
-    <a href="">
-        <img src="img/blog-3.jpg" class="card-img-top" alt="...">
-    </a>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-  </div>
-  <div class="card-footer">
-    <small class="text-muted tema">Saúde</small>
-    <small class="text-muted data ">21/09/2019</small>              
-</div>
-</div>
-</div>
-</div>
-</div>
-</section>
+		</div>
+	</nav>
 
-<section class="section-3">
-    <div class="container">
-        <div class="row">
-            <div class="box">                    
-                <div class="circulo"><a href="#"><i class="fas fa-chevron-left fa-1x"></i></a></div>
-                <div class="circulo"><a href="#">1</a></div>                    
-                <div class="circulo"><a href="#">2</a></div>                    
-                <div class="circulo"><a href="#">3</a></div>                    
-                <div class="circulo"><a href="#">4</a></div>                    
-                <div class="circulo"><a href="#">5</a></div>  
-                <div class="circulo"><a href="#"><i class="fas fa-chevron-right fa-1x"></i></a></div></div>
-            </div>
-        </div>
-    </section>
+	<div class="container">
+		<div class="row col-12">
+			<div class="col-6 ">
+				<div class="card">
+					<img src="img/elders-01.jpg">
+				</div>
+				<div class="col-6">
+					<p>  ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 
+	<footer class="btn-primary text-white py-2 mt-4">
+		<div class="container">
+			<div class="footer-copyright text-center py-3"> HCDigital © 2019 Copyright:</div>
+		</div>
+	</footer>
 
-    <!-- Roda pe -->
-    <footer class="btn-primary text-white py-2 mt-4">
-        <div class="container">
-            <div class="footer-copyright text-center py-3"> HCDigital © 2019 Copyright</div>
-        </div>
-    </footer>
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-    crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-    crossorigin="anonymous"></script>
-</body>
-
-</html>
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+		crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+		integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+		crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+		integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+		crossorigin="anonymous"></script>
+		<script src="https://kit.fontawesome.com/635b697ad0.js"></script>
+		<script type="js/blog.js"></script>
+	</body>
+	</html>
