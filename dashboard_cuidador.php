bdc2861fdd3e7ff8f47384f68d3b25b345568673
João Victor <brasil_connectint@hotmail.com>
2019-09-23 15:09:42 -0300

    Salvando modificações, faltando apenas a bolinha colorida

--- dashboard_cuidador.php
+++ dashboard_cuidador.php
@@ -15,52 +15,111 @@ if($site->session_error() == 0){
 <?php // Inclusão da NAVBAR lateral do sistema ?>
 <?php require_once('includes/navbar.php'); ?>
 
-    <div id="content">
+<div id="content">
 
-        <!-- Begin Page Content -->
-        <div class="container">
-            <div class="card my-5 ml-5" style=" width: 890px; height: 25%;">
-                
-                <div class="card-body float-left">
-                	<h5>#ID</h5>
-                	<hr>
-                    <i class="far fa-user-circle fa-3x float-right mr-5"></i>
-                     <p class="card-text">Nome do pasciente:</p>
-                    <p class="card-text">Tipo do serviço:</p>
+    <!-- Begin Page Content -->
+    <div class="container">
+        <div class="card my-5 ml-5 rounded-0" style=" width: 890px; height: 10%;">
+        <div class="card-body float-left">
+            <h5>#2760</h5>
+            <hr>
+            <div class="row">
+                <div class="col-4">
+                    <p class="card-text mr-5">Nome do pasciente: Alpha</p>  
                 </div>
-            </div>
+                <div class="col-4">
+                    <p class="card-text">Tipo do serviço: Cuidador de Idoso</p>
+                </div>
+                <div class="col-4">
+                  <button type="button" class="btn btn-primary  btn-sm ml-5">Ver detalhes</button>
+              </div>
+          </div>    
+      </div>
+  </div>
+  <div class="card my-5 ml-5 rounded-0" style=" width: 890px; height: 10%;">
+    <div class="card-body float-left">
+        <h5>#2760</h5>
+        <hr>
+        <div class="row">
+             <div class="col-4">
+            <p class="card-text">Nome do pasciente: Beta</p>  
+        </div>
+             <div class="col-4">
+            <p class="card-text">Tipo do serviço: Cuidador de Idoso</p>
+        </div>
+        <div class="col-4">
+                  <button type="button" class="btn btn-primary  btn-sm ml-5">Ver detalhes</button>
+              </div>
         </div>
     </div>
+</div>
+<div class="card my-5 ml-5 rounded-0" style=" width: 890px; height: 10%;">
+    <div class="card-body float-left">
+        <h5>#2760</h5>
+        <hr>
+        <div class="row">
+             <div class="col-4">
+            <p class="card-text">Nome do pasciente: Charlie</p>  
+        </div>
+             <div class="col-4">
+            <p class="card-text">Tipo do serviço: Cuidador Infantil</p>
+        </div>
+        <div class="col-4">
+                  <button type="button" class="btn btn-primary  btn-sm ml-5">Ver detalhes</button>
+              </div>
+        </div>
+    </div>
+</div>
+<div class="card my-5 ml-5 rounded-0" style=" width: 890px; height: 10%;">
+    <div class="card-body float-left">
+        <h5>#2760</h5>
+        <hr>
+        <div class="row">
+             <div class="col-4">
+            <p class="card-text">Nome do pasciente: Delta</p>  
+        </div>
+             <div class="col-4">
+            <p class="card-text">Tipo do serviço: Cuidador de Idoso</p>
+        </div>
+        <div class="col-4">
+                  <button type="button" class="btn btn-primary  btn-sm ml-5">Ver detalhes</button>
+              </div>
+        </div>
+    </div>
+</div>
 
-    <!-- SCRIPT PARA CONVERTER DEPOIS... -->
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var calendarEl = document.getElementById('calendar');
 
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                plugins: [ 'dayGrid' ],
-                defaultView: 'dayGridMonth',
-                fixedWeekCount: Boolean, default: true,
-                height: 370,
-                contentHeight: 4,
-                locale: 'pt',
-                events: [{
-                    id: 1,
-                    title: 'Serviço',
-                    start: new Date(2019, 8, 11),
-                    allDay: false,
-                    className: 'info'
-                }],
-                header: {
-                    center: '',
-                    right: 'today, prev, next'
-                }
 
-            });
 
-            calendar.render();
+<!-- SCRIPT PARA CONVERTER DEPOIS... -->
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            plugins: [ 'dayGrid' ],
+            defaultView: 'dayGridMonth',
+            fixedWeekCount: Boolean, default: true,
+            height: 370,
+            contentHeight: 4,
+            locale: 'pt',
+            events: [{
+                id: 1,
+                title: 'Serviço',
+                start: new Date(2019, 8, 11),
+                allDay: false,
+                className: 'info'
+            }],
+            header: {
+                center: '',
+                right: 'today, prev, next'
+            }
+
         });
-    </script>
+
+        calendar.render();
+    });
+</script>
 
 <?php // Inclusão da FOOTER do sistema ?>
 <?php require_once('includes/footer.php'); ?>
