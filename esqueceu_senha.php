f202a2077c839de0450063fd87d5a811246f9118
João Victor <brasil_connectint@hotmail.com>
2019-10-03 17:08:21 -0300

    Reformulando Token pra PHPMAILER (Em andamento)

--- esqueceu_senha.php
+++ esqueceu_senha.php
@@ -1,36 +1,36 @@
 <?php
 
-	require_once 'includes/startfile.php';
-	
-	require_once 'classes/login.class.php';
-	$login = new Login();
+require_once 'includes/startfile.php';
 
-	if (isset($_POST[btnEntrar])) {
-		$email = $mysqli->escape_string($_POST['email']);
+require_once 'classes/login.class.php';
+$login = new Login();
 
-		if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
-			$erro[] = "E-mail inválido.";
-		}
+if (isset($_POST[btnEntrar])) {
+	$email = $mysqli->escape_string($_POST['email']);
 
-		if (count($erro) == 0) {
+	if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+		$erro[] = "E-mail inválido.";
+	}
+
+	if (count($erro) == 0) {
 		
 		$novasenha = substr(md5(time(),0,6));
 		$nscriptografada = md5(md5($novasenha));
 		
 
-	if (mail($email, "Sua nova senha", "Sua nova senha:".$novasenha)){	
-		$sql_code = "UPDATE contratante SET senha = '$nscriptografada' WHERE email = '$email'";
-		$sql_query = $mysqli->query($sql_code) or die($mysqli->error);
+		if (mail($email, "Sua nova senha", "Sua nova senha:".$novasenha)){	
+			$sql_code = "UPDATE contratante SET senha = '$nscriptografada' WHERE email = '$email'";
+			$sql_query = $mysqli->query($sql_code) or die($mysqli->error);
 
 
+		}
 	}
-}
 
 }
 
 ?>
 
-	<title>HCD | Esqueceu sua senha</title>
+<title>HCD | Esqueceu sua senha</title>
 </head>
 
 <body style="background-color: dodgerblue">
