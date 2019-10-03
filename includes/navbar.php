ce214e3072c1d56ec4ffb5402a190cccfe4b7889
Nathan de Andrade <nathandeandrade21@gmail.com>
2019-10-03 15:48:30 -0300

    responsivo de troca de icone e responsivo

--- includes/navbar.php
+++ includes/navbar.php
@@ -17,6 +17,7 @@
 
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard_contratante.php">
         <div class="sidebar-brand-text mx-3">HCDigital</div>
+        <div class="sidebar-brand-text mx-3" id="hcd">HCD</div>
     </a>
 
     <!-- Divisor -->
@@ -25,7 +26,18 @@
     <!-- Perfil Info -->
     <div class="text-center text-white my-3">
         <img src="media/img/img-04.jpeg" class="rounded-circle profile-img d-inline" id="profile-img">
-        <p class="mb-0 mt-2"><?=$_SESSION['nome_usuario']?></p>
+        <p class="mb-0 mt-2" id="nome_perfil"><?=$_SESSION['nome_usuario']?></p>
+    </div>
+
+    <div class="text-center my-3">
+        <li class="nav-item">
+            <small>
+                <a href="editar_perfil.php" class="nav-link text-center p-0">
+                    <i class="far fa-fw fa-edit"></i>
+                    <span id="edi">Editar perfil</span>
+                </a>
+            </small>
+        </li>
     </div>
 
     <?php if($site->session_type() == 'contratante'){ ?>
@@ -33,8 +45,8 @@
         <li class="nav-item">
             <small>
                 <a href="dashboard_contratante.php" class="nav-link text-center p-0">
-                    <i class="far fa-fw fa-edit"></i>
-                    <span>Dashboard Contratante</span>
+                    <i class="fas fa-list"></i>
+                    <span>Dashboard</span>
                 </a>
             </small>
         </li>
@@ -46,30 +58,21 @@
         <li class="nav-item">
             <small>
                 <a href="dashboard_cuidador.php" class="nav-link text-center p-0">
-                    <i class="far fa-fw fa-edit"></i>
-                    <span>Dashboard Cuidador</span>
+                    <i class="fas fa-list"></i>
+                    <span>Dashboard</span>
                 </a>
             </small>
         </li>
     </div>
+
     <?php }?>
 
-    <div class="text-center my-3">
-        <li class="nav-item">
-            <small>
-                <a href="editar_perfil.php" class="nav-link text-center p-0">
-                    <i class="far fa-fw fa-edit"></i>
-                    <span>Editar perfil</span>
-                </a>
-            </small>
-        </li>
-    </div>
 
     <?php if($site->session_type() == 'contratante'){ ?>
     <div class="text-center my-3">
         <li class="nav-item">
             <a href="solicitar_servico.php" class="nav-link text-center p-0">
-                <i class="far fa-fw fa-comments"></i>
+                <i class="fas fa-search-dollar"></i>
                 <span>Solicitar Serviço</span>
             </a>
         </li>
@@ -80,8 +83,8 @@
     <div class="text-center my-3">
         <li class="nav-item">
             <a href="servicos_disponiveis.php" class="nav-link text-center p-0">
-                <i class="far fa-fw fa-comments"></i>
-                <span>Serviços Disponíveis</span>
+               <i class="fas fa-briefcase"></i>
+                <span id="soli">Serviços Disponíveis</span>
             </a>
         </li>
     </div>
@@ -91,7 +94,7 @@
         <li class="nav-item no-arrow mb-0">
             <a href="#" class="nav-link text-center" data-toggle="modal" data-target="#logoutModal">
                 <i class="fas fa-fw fa-sign-out-alt"></i>
-                <span>Sair</span>
+                <span id="sair">Sair</span>
             </a>
         </li>
     </div>
